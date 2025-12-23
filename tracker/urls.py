09571e7d9df9f55bb00bbedf73d79f04c3c2c801
Cma <chicchaima@gmail.com>
2025-12-23 18:44:50 +0900

    for winter 2025

--- tracker/urls.py
+++ tracker/urls.py
@@ -81,6 +81,11 @@ urlpatterns = [
         donateviews.paypal_return_msf2025s,
         name='paypal_return_msf2025s',
     ),
+    path(
+        'paypal_return/msf2025w',
+        donateviews.paypal_return_msf2025w,
+        name='paypal_return_msf2025w',
+    ),
     path('paypal_cancel/', donateviews.paypal_cancel, name='paypal_cancel'),
     path('ipn/', donateviews.ipn, name='ipn'),
     path('analytics/', analyticsviews.post_analytics, name='analytics'),
